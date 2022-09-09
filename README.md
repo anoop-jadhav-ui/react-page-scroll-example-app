f13f7bb1602fcb69ce02711ee82982fe5f79c57c
Anoopjadhav <anoopjadhav@gmail.com>
2022-09-09 11:54:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-qmmsys
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qmmsys)
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useRef } from 'react';
+import RandomThings from './RandomThings.js';
+import PageScrollProgressBar from 'react-page-scroll-progress-bar';
+
+const App = () => {
+  const AppRef = useRef(null);
+
+  return (
+    <>
+      <PageScrollProgressBar
+        AppRef={AppRef}
+        progressBarColor="red"
+        progressBarBgColor="#f2f2f2"
+        progressBarHeight="6px"
+      />
+      <div className="App" ref={AppRef}>
+        <RandomThings />
+      </div>
+    </>
+  );
+};
+
+export default App;
